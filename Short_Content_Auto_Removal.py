c27da6f52d52deb8e087b5e0d13ff9e4302afbd6
asoccer <reainasky@gmail.com>
2015-06-18 04:24:16 -0400

    Changed the Duration handling

--- Short_Content_Auto_Removal.py
+++ Short_Content_Auto_Removal.py
@@ -1,21 +1,29 @@
-import praw,pafy
+import praw,pafy,sys,time
 
-r = praw.Reddit("Approve/Remove Short Duration Content")
-r.login('picflute','RX4RQheqVu5x')
-
-submission = praw.helpers.submission_stream(r,'Riven',limit=10,verbosity = 1)
-video_length = []
-for a in submission:
-    print(a.url)
-    if 'youtube' in a.url:
-        video = pafy.new(a.url)
-        for i in range(0,8,3):
-            video_length.append(video.duration[0+i:2+i])
-        if(video_length[0] == "00" and video_length[1] == "00"):
-                length = int(video_length[2])
-                if length<30:
-                    a.remove()
-        video_length = []
-    else:
-        print('not YT')
-        
+def reddit_login():
+    submission = praw.helpers.submission_stream(r,'leagueoflegends',limit=10,verbosity = 1)
+    video_length = []
+    for a in submission:
+        if 'youtube' in a.url or 'youtu.be' in a.url:
+            try:
+                video = pafy.new(a.url)
+                duration = str(video.duration).replace(":","")
+                #Is it even 30s?
+                if '0000' == duration[0:4]:
+                    if int(duration[4]) <3:
+                        wiki = r.get_wiki_page('leagueoflegends','shortduration')
+                        wiki.edit(wiki.content_md + '\n\n ' + a.id)
+                        a.remove()
+                        a.add_comment("Hello, your post has been removed because it is in violation of our short content rule. All Content <30s must be submitted as a self post. This action was done automatically. If you beleive this removal was incorrent immediately [Message the mods](http://www.reddit.com/message/compose?to=%2Fr%2Fleagueoflegends)").distinguish()
+            except Exception as e:
+                a.report("Picflutes Bot Could not understand the duration of this video")
+                print(e)
+        else:
+            pass
+                    
+                                    
+if __name__ == '__main__':
+    r = praw.Reddit("Approve/Remove Short Duration Content")
+    r.login()
+    reddit_login()
+    
