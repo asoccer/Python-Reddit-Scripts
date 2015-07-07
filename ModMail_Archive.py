6672311d56941f0c4c4d9a2ac4a25a0b60e399a4
asoccer <reainasky@gmail.com>
2015-07-07 16:43:21 -0400

    because i hate OAuth and don't want to migrate yet

--- ModMail_Archive.py
+++ ModMail_Archive.py
@@ -1,25 +1,40 @@
-import praw,time
+import praw
+import OAuth2Util
 
-r = praw.Reddit("Archive ModMail Bot")
-message = 'https://www.reddit.com/message/messages/'
-after_field =""
-modmail = r.get_mod_mail('leagueoflegends',limit = 10000)
-modid = []
-content = {'subject':[],'author': [],'dest':[],'id':[],'time':[]}
+#r = praw.Reddit("My Dank Meme's Bot")
+#o = OAuth2Util.OAuth2Util(r)
+
+import praw
+import time
+#Converts UTC To something readable
+def t(user):
+    created = user.created_utc
+    date = time.ctime(created)
+    return date
+
+#Set HTTPS off in your account settings if you've never done PRAW before. This doesn't work if you have it on.
+#And you can re-enable it after running this.
+
+r = praw.Reddit("ModMail Archive action")
+f = open("ModMailDemo.txt","w") #Create and Write to
+f.write("Author | Subject | Permalink |Date\n---|---|----|----\n") #You're going to be posting the dump in the Wiki and don't need to doublespace
+USER = input('Type User Name\n')
+PASSWORD = input('Type Your Password | NOTE THIS DOESNT SEND OR SAVE THE PASSWORD ANYWHERE\n')
+SUBREDDIT = input('Input the Subreddit You Wish to archive | YOU MUST HAVE MAIL PRIVS TO DO THIS')
+r.login(USER,PASSWORD) #Use your damn creds "dankflute","123456"
+
+redd = "https://www.reddit.com/message/messages/"
+print("Searching in the database....")
+modmail = r.get_mod_mail(SUBREDDIT,params = None,limit = None)
+count =0
 for a in modmail:
-    content['author'].append(str(a.author))
-    content['subject'].append(str(a.subject))
-    t = time.ctime(int(a.created_utc))
-    content['time'].append(str(t))
-    content['id'].append(str(message + a.id))
-    content['dest'].append(str(a.dest))
-                           
+    if(str(a.author)!="AutoModerator"):
+        try:
+            count+=1
+            f.write(str(a.author) + " | "+ str(a.subject) + " | " + redd+ str(a.id)+ " | " + t(a)+"\n")
+        except Exception as e:
+             
+            pass #Weird Unicode Errors I got that I didn't want to waste time on handling. Someone else wants to fix them do it.
 
-
-f = open('write.txt','w')
-for a in range(len(content['author'])):
-    try:
-        f.write(str(content['author'][a]) + "|" +str(content['dest'][a]) + "|" + content['subject'][a] + '|' + content['id'][a] + "|" + str(content['time'][a]) + "\n")
-    except:
-        print(str(content['author'][a]) + "|" +str(content['dest'][a]) + "|" + content['subject'][a] + '|' + content['id'][a] + "|" + str(content['time'][a]))
-        print("this didn't load")
+f.close()
+print("Completed, please check your Textfile\nYou had a total of " + str(count) + "messages found god bless\n Message /u/picflute if you have any errors or improvements and I'll be happy to listen")
